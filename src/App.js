c8a4c81f566fd3e2edfdd50b4257050272a68123
Nick <nickmagi24@gmail.com>
2021-10-28 13:02:47 -0400

    Refactor to class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ToDoItem from "./TodoItem";
 import todosData from "./todosData";
 
 import './style.css';
 
 
-function App() {
-  const todoItems = todosData.map((item) => <ToDoItem key={item.id} item={item} />);
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todos: todosData
+    }
+
+  }
+
+  render() {
+    
+    const todoItems = this.state.todos.map((item) => <ToDoItem key={item.id} item={item} />);
 
     return (
         <div className="todo-list">
           {todoItems}
         </div>
-    )
+      )
+  }
 }
 
 export default App;
