0ea7bf2fbb114385c8a05615f26f0769877b0bd8
Nick <nickmagi24@gmail.com>
2021-11-09 14:13:03 -0500

    TodoForm Component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
-import React, { Component } from "react";
-import ToDoItem from "./TodoItem";
-import todosData from "./todosData";
+import React, { Component } from 'react';
+import TodoForm from './components/TodoForm';
 
-import './style.css';
-
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      todos: todosData
+export class App extends Component {
+    constructor(props) {
+        super(props)
     }
-    this.handleChange = this.handleChange.bind(this);
-  }
 
-  handleChange(id) {
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed
-          }
-        } 
-        return todo;
-      })
-      return {
-        todos: updatedTodos
-      }
-    })
-  }
-  
 
-  render() {
-    const todoItems = this.state.todos.map((item) => 
-      <ToDoItem 
-        key={item.id} 
-        item={item} 
-        handleChange={this.handleChange} 
-          S
-        />);
-
-    return (
-        <div className="todo-list">
-            {/* <form>
-              <input 
-                type='text'
-                value={this.item}
-                placeholder='Add Item' 
-                onChange={this.handleChange} />
-            </form> */}
-          {todoItems}
-        </div>
-      )
-  }
+    render() {
+        return (
+            <div className='todo-app'>
+                <TodoForm />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
